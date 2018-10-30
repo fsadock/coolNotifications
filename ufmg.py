2cde31cd40209c5b6513a45ccfdb1574ffc8efa5
Sadog <fsadock@gmail.com>
2018-10-30 15:19:00 -0300

    changed requirements and sleep time

--- ufmg.py
+++ ufmg.py
@@ -1,9 +1,7 @@
 import requests
-from bs4 import BeautifulSoup
 from time import sleep, strftime
-import re
 from pushbullet import Pushbullet
-import os, sys
+import os
 
 headers = {
     'headers': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
@@ -15,7 +13,7 @@ r = requests.get(url, headers=headers, timeout=5)
 while r.status_code != 200:
     time = strftime('%X')
     print(time, r.status_code)
-    sleep(86400)
+    sleep(28800)
 
 print("Saiu o edital!")
 os.system('ntfy -t "SITE UP!!" send "%s"' % url)
