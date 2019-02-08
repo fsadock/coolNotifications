f3c52f03d64ebb5f159db4c146cf5f2d548b1160
Felipe Sadock <fsadock@gmail.com>
2019-02-08 03:05:49 -0200

    Update ufmg.py

--- ufmg.py
+++ ufmg.py
@@ -32,8 +32,8 @@ def checkWebPage():
 
     headers = {
         'headers': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'}
-    url = "https://www.ufmg.br/copeve/Arquivos/2018/trob_edital_ufmg2019.pdf"
-    pb = Pushbullet("o.XlhLt0s4KzfMsQg0oDwFF98IkAAzD3tG")
+    url = "Website" #the website's url 
+    pb = Pushbullet("yourpbKey") # used pushbullet to receive notifications on my phone
     r = requests.get(url, headers=headers, timeout=5)
 
     time = strftime('%X')
@@ -43,9 +43,9 @@ def checkWebPage():
 
     print(time, r.status_code)
     if r.status_code == 200:
-        print("Saiu o edital!")
+        print("Tudo pronto!")
         # os.system('ntfy -t "SITE UP!!" send "%s"' % url)
-        pb.push_note("UFMG", "Saiu EDITAL")
+        pb.push_note("title", "description") #notify the user
 
 
 if __name__ == '__main__':
